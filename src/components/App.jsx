6b4a0f3f737a9f3e75b3d8006d0452fe354d6e15
Volin13 <110535249+Volin13@users.noreply.github.com>
2023-01-11 22:25:56 +0200

    commit message1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
+import {
+  Section,
+  Filter,
+  Contacts,
+  AddContactForm,
+  Container,
+} from '../components/index';
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { fetchContacts } from '../redux/services/services';
+import { selectIsLoading } from '../selectors/selectors';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const onSucsessMessage = message => toast.success(message);
+export const onErrorMessage = message => toast.error(message);
+
+const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container title="Phone book">
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {isLoading && <h2 style={{ margin: '0 auto' }}>Loading...</h2>}
+      <Section title="Add new contact" variant="h3">
+        <AddContactForm />
+      </Section>
+      <Section title="Your contacts" variant="h3">
+        <Contacts>
+          <Filter />
+        </Contacts>
+      </Section>
+    </Container>
   );
 };
+export default App;
